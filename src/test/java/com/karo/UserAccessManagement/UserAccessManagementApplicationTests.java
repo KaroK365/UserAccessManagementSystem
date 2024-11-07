0edf8f6d88af9f7bda9241cbe9bd004eff1b6a84
Karok365 <anshsinghh365@gmail.com>
2024-11-07 13:14:50 +0530

    Initial Commit

---
+++ src/test/java/com/karo/UserAccessManagement/UserAccessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karo.UserAccessManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
